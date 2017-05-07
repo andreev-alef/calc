26c00967ce24aead91e016e473b735ff64ef7728
andreev_af <andreev.alef@gmail.com>
2017-05-08 01:19:27 +0700

    Файлы html загружаются как шаблоны

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 # -*-coding: utf-8-*-
 import sys
 
+import tornado
 import tornado.ioloop
 import tornado.web
+import string
 
 
 # print "Content-Type: text/html"     # HTML is following
@@ -18,12 +20,22 @@ import tornado.web
 
 class Index(tornado.web.RequestHandler):
     def get(self):
-        self.write("Python version: %s" % sys.version)
+        indexTemplate = string.Template(open("./static/index.html", "r").read())
+        self.write(indexTemplate.substitute())
+
+
+class Plan(tornado.web.RequestHandler):
+    def get(self):
+        indexTemplate = string.Template(open("./static/plan.html", "r").read())
+        self.write(indexTemplate.substitute())
 
 
 def makeApp():
     return tornado.web.Application([
+        (r"/lib/(.*)", tornado.web.StaticFileHandler, {"path": "./static/lib"}),
         (r"/", Index),
+        (r"/plan", Plan)
+        # (r"/(.*)", tornado.web.StaticFileHandler, {"path":"D:/www/PycharmProjects/calc/static"})
     ])
 
 
