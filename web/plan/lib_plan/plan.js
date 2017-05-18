c2f688a77a684ce07908fe8bba605040c53719cf
andreev_af <andreev.alef@gmail.com>
2017-05-18 17:53:04 +0700

    Эксперимент с быстрой фильтрацией.

--- web/plan/lib_plan/plan.js
+++ web/plan/lib_plan/plan.js
@@ -4,10 +4,17 @@
  * and open the template in the editor.
  */
 
-var orderByDimension = '';
-var orderByDimensionFlag = true;
-var previousColumn = '';
-var POSTparam = {};
+let orderBy = 'calcid';
+let orderByDimension = '';
+let orderByDimensionFlag = true;
+let previousColumn = '';
+let POSTparam = {};
+let url = '/calc';
+let dataType = 'html';
+
+function setInfo(info) {
+    $("#info").html(info);
+}
 
 function comma2dot(sourceString) {
     let strComma = sourceString.toString();
@@ -18,6 +25,49 @@ function dot2comma(sourceString) {
     let strDot = sourceString.toString();
     let strComma = strDot.replace('.', ',');
     return strComma;
+}
+
+function writeTable(tRows = {}) {
+    let tableHeader = '<table class="table table-striped table-bordered rows" id="view_all">' +
+            '<tr class="row_header">' +
+            '<th class="column_header">&nbsp;</th>' +
+            '<th class="column_header" data-column_name="calcid" onclick="calcShowAll(this);">&nbsp;№&nbsp;</th>' +
+            '<th class="column_header" data-column_name="calc_date" onclick="calcShowAll(this);">Дата</th>' +
+            '<th class="column_header" data-column_name="title" onclick="calcShowAll(this);">Наименование</th>' +
+            '<th class="column_header" data-column_name="tirazh" onclick="calcShowAll(this);">Тираж</th>' +
+            '<th class="column_header" data-column_name="cena_knigi" onclick="calcShowAll(this);">Стоимость книги</th>' +
+            '<th class="column_header" data-column_name="cena_na_tirazh_nds" onclick="calcShowAll(this);">Стоимость тиража</th></tr>';
+    let tableFooter = '</table>';
+    let rows = tRows;
+    let i = 0;
+    let N = rows.row.length;
+    let tableBody = '';
+    for (i = 0; i < N; i++) {
+        tableBody += '<tr class="calc_row" data-id="' + rows.row[i].calcid + '" onclick="openCalc(this);">' +
+                '<td class="print"><img class="btnPrint" src="../lib/img/print.ico" data-id=' + rows.row[i].calcid + ' alt="Распечатать" onclick="printCalc(this, event);" /></td>' + //onclick="printCalc(this);"
+                '<td class="calcid">' + rows.row[i].calcid + '</td>' +
+                '<td class="calcdate">' + rows.row[i].calcdate + '</td>' +
+                '<td class="title">' + rows.row[i].title + '</td>' +
+                '<td class="tirazh">' + rows.row[i].tirazh + '</td>' +
+                '<td class="cena_knigi">' + dot2comma(Number.parseFloat(rows.row[i].cena_knigi).toFixed(2)) + '</td>' +
+                '<td class="cena_na_tirazh_nds">' + dot2comma(Number.parseFloat(rows.row[i].cena_na_tirazh_nds).toFixed(2)) + '</td>' +
+                '</tr>';
+    }
+    $('.wr-data').html(tableHeader + tableBody + tableFooter);
+}
+
+function getOrderBy(element) {
+    if ($(element).data('column_name') === undefined) {
+        orderBy = "calcid";
+    } else {
+        orderBy = $(element).data('column_name');
+    }
+    orderByDimension = orderByDimension === 'asc' ? 'desc' : 'asc';
+    if (previousColumn !== orderBy) {
+        orderByDimension = 'asc';
+    }
+    previousColumn = orderBy;
+    return {'colName': orderBy, 'dimension': orderByDimension};
 }
 
 function openCalc(e) {
@@ -32,72 +82,55 @@ function printCalc(element, e) {
     e.stopPropagation();
 }
 
-function showError(errorData) {
-    $('.wr-data').html('<div class="alert alert-danger" role="alert">' + exception.toString() + '</div>');
+function calcShowAll(element) {
+
+    if ($(element).data('column_name') === undefined) {
+        orderBy = "calcid";
+    } else {
+        orderBy = $(element).data('column_name');
+    }
+    orderByDimension = orderByDimension === 'asc' ? 'desc' : 'asc';
+    if (previousColumn !== orderBy) {
+        orderByDimension = 'asc';
+    }
+    previousColumn = orderBy;
+    POSTparam = {
+        "command": "get_all",
+        "orderBy": orderBy,
+        "orderByDimension": orderByDimension
+    };
+
+    $.post(url, POSTparam, function (data) {
+        writeTable(JSON.parse(data));
+    }, dataType);
+
 }
 
-function calcShowAll(element) {
+function calcShowCurrentYear() {
     try {
-        let url = '/calc';
-        let dataType = 'html';
-        let orderBy = 'calcid';
-
-        if ($(element).data('column_name') === undefined) {
-            orderBy = "calcid";
-        } else {
-            orderBy = $(element).data('column_name');
-        }
-
-
-        orderByDimension = orderByDimension === 'asc' ? 'desc' : 'asc';
-        if (previousColumn !== orderBy) {
-            orderByDimension = 'asc';
-        }
-        POSTparam = {
-            "command": "get_all",
-            "orderBy": orderBy,
-            "orderByDimension": orderByDimension
-        };
-        let rows = {};
-        let tableHeader = '<table class="table table-striped table-bordered rows" id="view_all">' +
-                '<tr class="row_header">' +
-                '<th class="column_header">&nbsp;</th>' +
-                '<th class="column_header" data-column_name="calcid" onclick="calcShowAll(this);">&nbsp;№&nbsp;</th>' +
-                '<th class="column_header" data-column_name="calc_date" onclick="calcShowAll(this);">Дата</th>' +
-                '<th class="column_header" data-column_name="title" onclick="calcShowAll(this);">Наименование</th>' +
-                '<th class="column_header" data-column_name="tirazh" onclick="calcShowAll(this);">Тираж</th>' +
-                '<th class="column_header" data-column_name="cena_knigi" onclick="calcShowAll(this);">Стоимость книги</th>' +
-                '<th class="column_header" data-column_name="cena_na_tirazh_nds" onclick="calcShowAll(this);">Стоимость тиража</th></tr>';
-        let tableFooter = '</table>';
-
-        $.post(url, POSTparam, function (data) {
-            try {
-                rows = JSON.parse(data);
-                let i = 0;
-                let N = rows.row.length;
-                let tableBody = '';
-                for (i = 0; i < N; i++) {
-                    tableBody += '<tr class="calc_row" data-id="' + rows.row[i].calcid + '" onclick="openCalc(this);">' +
-                            '<td class="print"><img class="btnPrint" src="../lib/img/print.ico" data-id=' + rows.row[i].calcid + ' alt="Распечатать" onclick="printCalc(this, event);" /></td>' + //onclick="printCalc(this);"
-                            '<td class="calcid">' + rows.row[i].calcid + '</td>' +
-                            '<td class="calcdate">' + rows.row[i].calcdate + '</td>' +
-                            '<td class="title">' + rows.row[i].title + '</td>' +
-                            '<td class="tirazh">' + rows.row[i].tirazh + '</td>' +
-                            '<td class="cena_knigi">' + dot2comma(Number.parseFloat(rows.row[i].cena_knigi).toFixed(2)) + '</td>' +
-                            '<td class="cena_na_tirazh_nds">' + dot2comma(Number.parseFloat(rows.row[i].cena_na_tirazh_nds).toFixed(2)) + '</td>' +
-                            '</tr>';
-                }
-                $('.wr-data').html(tableHeader + tableBody + tableFooter);
-            } catch (exception) {
-                $('.wr-data').html('<div class="alert alert-danger" role="alert">' + exception.toString() + '<br />' + data + '</div>');
-            }
-        }, dataType);
-        previousColumn = orderBy;
-    } catch (exception) {
-        alert(exception.toString());
-        $('.wr-data').html('<div class="alert alert-danger" role="alert">' + exception.toString() + '</div>');
+        let D = new Date();
+        setInfo(D.getFullYear());
+    } catch (e) {
+        alert(e.toString());
     }
 }
+function calcShowCurrentMonth() {
+    try {
+        let D = new Date();
+        setInfo(D.getMonth());
+    } catch (e) {
+        alert(e.toString());
+    }
+}
+function calcShowCurrentDay() {
+    try {
+        let D = new Date();
+        setInfo(D.getDate());
+    } catch (e) {
+        alert(e.toString());
+    }
+}
+
 function calcShowRowCount() {
     try {
         var url = '/calc';
@@ -117,8 +150,8 @@ $(document).ready(function () {
     calcShowAll();
     calcShowRowCount();
 
-    $('.btnPrint').click(function () {
-        printCalc(this);
-    });
+    $('#calcShowCurrentYear').click(calcShowCurrentYear);
+    $('#calcShowCurrentMonth').click(calcShowCurrentMonth);
+    $('#calcShowCurrentDay').click(calcShowCurrentDay);
 }
 );
